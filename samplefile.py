f3d2b627e90df4283ce97f2c70de432d9ed49674
sreesuja 18 <sreesujabs@gmail.com>
2026-03-04 18:51:50 +0530

    added python sample

---
+++ samplefile.py
@@ -0,0 +1,14 @@
+print("Welcom to python world")
+
+username="Sree"
+age=21
+is_active=True
+value=1233.00
+print(username)
+print(age)
+print(is_active)
+
+print(type(username))
+print(type(age))
+print(type(is_active))
+print(type(value))
